b8a79ba75dddc6e5388692fdbc581e53e049b4c0
nish@cs.ucla.edu <ubuntu@ip-172-31-28-11.us-west-1.compute.internal>
2016-10-22 06:04:41 +0000

    Util to extract images from an ros bag file

---
+++ extract_images.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import cv2
+import os
+import roslib
+import rosbag
+import rospy
+import sys
+
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+
+def getTime(msg):
+  return (msg.header.stamp.secs*1000) + (msg.header.stamp.nsecs/1000000)
+
+def extract_images(bagfile, topicname, outputdir, compressed):
+  bag = rosbag.Bag(bagfile)
+  bridge = CvBridge()
+  index = 1
+  for topic, msg, t in bag.read_messages(topics=[topicname]):
+    ti = (getTime(msg) / 50) * 50
+    if compressed:
+      cv_image = bridge.compressed_imgmsg_to_cv2(msg)
+    else:
+      cv_image = bridge.imgmsg_to_cv2(msg)
+    cv2.imwrite(outputdir + str(index) + ".jpg", cv_image)
+    print ti
+    index = index + 1
+  bag.close()
+
+if __name__ == '__main__':
+  if len(sys.argv) <= 1:
+    print "python extract_images.py <bagfile path> <topicname> <outputdir to persist images> <compressed>"
+    sys.exit()
+  bagfile = sys.argv[1]
+  topicname = sys.argv[2]
+  outputdir = sys.argv[3] 
+  os.makedirs(outputdir) 
+  compressed = False
+  if len(sys.argv) == 5 and sys.argv[4] == 'True':
+    compressed=True
+  extract_images(bagfile, topicname, outputdir, compressed)
+  
